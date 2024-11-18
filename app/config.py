4f51a64467917033c5d2bd95c11be40863d2fc99
yuniesss <879766866@qq.com>
2024-11-18 23:59:07 +0800

    11.18save

--- app/config.py
+++ app/config.py
@@ -4,7 +4,16 @@ class Config(object):
 class DevConfig(Config):
     DEBUG = True
     #这里是使用的数据库信息
+    DIALECT = 'mysql'
+    DRIVER = 'pymysql'
+    USERNAME = 'root'
+    PASSWORD = '****'
+    HOST = '127.0.0.1'
+    PORT = '3306'
+    DATABASE = '数据库名'
     #DATABASE_URI = '??? ??? ???'
 
 class ProConfig(Config) :
     DEBUG = False
+
+settings = DevConfig()
